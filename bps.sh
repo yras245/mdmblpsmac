9d3ab0ece636256936ec24a36324c169393b24fd
yras245 <65346057+yras245@users.noreply.github.com>
2024-01-23 19:27:22 +0200

    Update bps.sh

--- bps.sh
+++ bps.sh
@@ -64,7 +64,7 @@ echo -e "${RED}*             UO5OQ.COM                 *${NC}"
 echo -e "${RED}*              wins94                    *${NC}"
 echo -e "${CYAN}*-------------------*---------------------*${NC}"
 echo ""
-
+PS3='Перед работой с скриптом убедитесь что ваш диск переименован в Macintosh HD а раздел с данными в Macintosh HD - Data'
 PS3='Please enter your choice: '
 options=("Autoypass on Recovery" "Reboot" "Exit")
 
